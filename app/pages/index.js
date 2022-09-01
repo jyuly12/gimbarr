3daae428970de20bda8878e44b8a541858df59c4
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-09-01 11:53:39 -0500

    Add initial files

---
+++ app/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Gimbarr</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    </div>
+  )
+}
