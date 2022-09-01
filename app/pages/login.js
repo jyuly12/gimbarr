dfe35a0d9de5959f1c495b200b691e9bac0a2116
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-09-01 12:11:10 -0500

    delete style

---
+++ app/pages/login.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+const Login = () => {
+    return (
+        <div>
+            <div>
+                image
+            </div>
+            <div>
+                <form action="">
+                    <p></p>
+                    <div>
+                        <label htmlFor=""></label>
+                        <input type="text" name="" id="" />
+                    </div>
+                    <div>
+                        <label htmlFor=""></label>
+                        <input type="text" />
+                    </div>
+                    
+                    <p></p>
+                    <button></button>
+                </form>
+            </div>
+        </div>
+    )
+}
