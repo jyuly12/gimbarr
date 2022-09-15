8ff095485d08d7963cc351413debc61db57b4866
jyuly12 <yuly.gonzalez2021@gmail..com>
2022-09-15 08:21:05 -0500

    login update

--- gimbarr-app/pages/auth/login.tsx
+++ gimbarr-app/pages/auth/login.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import Input from "../../components/input";
 import Loginlayout from "../../components/loginlayout";
-import Image from 'next/image'
 import { useRouter } from 'next/router';
-import useUser from "../../lib/useUser";
-import styles from '../styles/signin.module.css'
 import { useSession, signIn, getCsrfToken, getProviders } from "next-auth/react";
 
 interface sessionprovider {
@@ -24,11 +21,8 @@ export default function Login( props:sessionprovider) {
   }
   return (
       <>
-        {/* {!user ? (<h1>Loading....</h1>) : 
-          <>
-          {!user.isLoggedIn &&  */}
                 
-        <Loginlayout>
+        <Loginlayout title="Gimbarr log in">
           <form method="post" action="/api/auth/signin/email">
             <div>
             <input name="csrfToken" type="hidden" defaultValue={props.csrfToken} />
@@ -43,8 +37,8 @@ export default function Login( props:sessionprovider) {
             </div>
 
             <div className="mt-6"> 
-              {/* Password */}
-              <Input 
+              {/* Password  later interation*/}
+              {/* <Input 
                 name="password" 
                 placeholder="insert your password" 
                 label="password"
@@ -52,28 +46,30 @@ export default function Login( props:sessionprovider) {
                 rightroute="#"
                 type="password"
                 id="password"
-              />
+              /> */}
             </div> 
 
-            <div className="mt-6">
+            <div className="my-6">
               <button type="submit"
                 className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-                Sign in
+                Sign in with magic link
               </button>
             </div>
           </form>
-
+          <hr className="mb-10" />
           {props.providers &&
               Object.values(props.providers).map(provider => (
-                <div key={provider?.name} style={{ marginBottom: 0 }}>
+                
+                <div key={provider?.name } style={{ marginBottom: 0 }}>
+                  { provider?.name === 'Google' && (
                   <button className="my-2 border flex justify-center w-full py-2  rounded-lg" onClick={() => signIn(provider?.id)} >
                     Sign in with{' '} {provider?.name}
-                  </button>
+                  </button>)}
                 </div>
               ))}
 
-
-          <form method="post" action="/api/auth/callback/credentials">
+                {/* credentials for later implementation */}
+         {/*  <form method="post" action="/api/auth/callback/credentials">
             <input name="csrfToken" type="hidden" defaultValue={props.csrfToken} />
             <label>
               Username
@@ -84,7 +80,11 @@ export default function Login( props:sessionprovider) {
               <input name="password" type="password" />
             </label>
             <button type="submit">Sign in</button>
-          </form>
+          </form> */}
+
+        {/* redirection to register for later implementation */}
+         {/*  <p className="mt-6 text-sm text-center text-gray-400">Don&#x27;t have an account yet? <a href="/auth/register" className="text-blue-500 focus:outline-none focus:underline hover:underline">Sign up</a>.</p> */}
+
         </Loginlayout>              
       </>
     )
