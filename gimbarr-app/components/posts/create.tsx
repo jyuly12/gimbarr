4ca5af9490ae21030a7139c6d60d6f2c23c1aa25
jyuly12 <yuly.gonzalez2021@gmail..com>
2022-09-15 08:13:54 -0500

    create post component and api

---
+++ gimbarr-app/components/posts/create.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import Router from "next/router";
+
+const CreatePost: React.FC = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const submitData = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    try {
+      const body = { title, content };
+      await fetch(`/api/post`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      await Router.push("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <form onSubmit={submitData}>
+
+          <h1>New Draft</h1>
+
+          {/* Post Title */}
+          <input
+            autoFocus
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Title"
+            type="text"
+            value={title}
+            className="w-full p-2 mt-2 border-4 border-gray-300 rounded-md"
+          />
+
+          {/* Post description */}
+          <textarea
+            cols={50}
+            onChange={(e) => setContent(e.target.value)}
+            placeholder="Content"
+            rows={8}
+            value={content}
+            className="w-full p-2 mt-2  border-4 border-gray-300 rounded-md"
+          />
+
+          {/* Create Button */}
+          <input disabled={!content || !title} type="submit" value="Create" 
+          className="bg-gray-200  py-4 px-8"/>
+          
+          {/* when cancel redirect to*/}
+          <a className="ml-4" href="#" onClick={() => Router.push("/")}>
+            or Cancel
+          </a>
+        </form>
+
+      </div>
+    </>
+  );
+};
+
+export default CreatePost;
