6d37db231d21d03a7f093b35c1dfbd08468c4f4a
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-09-15 13:06:40 -0500

    Add response files

--- gimbarr-app/components/posts/Index.tsx
+++ gimbarr-app/components/posts/Index.tsx
@@ -1,31 +1,50 @@
 import React from "react";
 import Router from "next/router";
+import Image from "next/image"
+import UserDefault from "../../assets/UserDefault.png"
 import ReactMarkdown from "react-markdown";
-
+import { videoURL } from "../../lib/video";
 export type PostProps = {
   id: number;
+  videoclip: string;
   title: string;
   author: {
     name: string;
     email: string;
+    image: string;
   } | null;
   content: string;
+  
   published: boolean;
 };
 
 const Index: React.FC<{ post: PostProps }> = ({ post }) => {
   const authorName = post.author ? post.author.name : "Unknown author";
+  const authorImg = post.author ? post.author.image : UserDefault;
+
+  const videourl = videoURL(post.videoclip)
+
   return (
-    <div className="bg-white hover:shadow-xl  my-2" /* onClick={() => Router.push("/post/[id]", `/post/${post.id}`)} */>
-      <h2>{post.title}</h2>
-      <small>By {authorName}</small>
-      <ReactMarkdown children={post.content} />
-      <style jsx>{`
-        div {
-          color: inherit;
-          padding: 2rem;
-        }
-      `}</style>
+    <div className="bg-white hover:shadow-md rounded-xl p-8 my-4" /* onClick={() => Router.push("/post/[id]", `/post/${post.id}`)} */>
+      
+      <div className="overflow-hidden mx-auto w-[80%] mb-2">
+        <h2 className="font-semibold text-lg uppercase">{post.title}</h2>
+        <iframe src={videourl} className="w-full h-[26rem] my-2" />
+      </div>
+      <div className="w-[80%] mx-auto">
+        
+        <div className="flex flex-row ">
+          <div className="w-6 h-6 relative bg-slate-50 border rounded-full ">
+            <Image alt="user profile" src={ authorImg } layout='fill' className="border rounded-full self-center"/>
+          </div>
+          <p className="">By {authorName}</p>
+        </div>
+        <div className="text-md mt-2">
+          <p className="font-semibold">Description:</p>
+          <ReactMarkdown children={post.content} className="text-sm capitalize" />
+        </div>
+      </div>
+      
     </div>
   );
 };
--- gimbarr-app/pages/index.tsx
+++ gimbarr-app/pages/index.tsx
@@ -1,16 +1,10 @@
 import { useSession, signIn, signOut } from "next-auth/react"
-import youtube_parser from '../lib/idVideo'
 import SlideBar from '../components/slidebar'
 import type { GetServerSideProps } from "next";
 import prisma from '../lib/prisma'
-
-
-import link from "next/link"
-
 import IndexPost, { PostProps } from "../components/posts/Index";
+import { videoURL } from "../lib/video";
 
-
-const youtubeUrl = "https://www.youtube.com/watch?v=Pw-0pbY9JeU&list=RDMM&start_radio=1&rv=qbLPmZCX7q0&ab_channel=twentyonepilots"
 
 /* browse published videos */
 export const getServerSideProps: GetServerSideProps = async () => {
@@ -22,6 +16,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
       author: {
         select: {
           name: true,
+          image: true,
         },
       },
     },
@@ -38,9 +33,8 @@ type Props = {
 export default function Index(props:Props) {
 
   /* videojs */
-  const videoId = youtube_parser(youtubeUrl)
-  const video = "https://www.youtube.com/embed/"+ videoId
-
+  const youtubeUrl = "https://www.youtube.com/watch?v=jfKfPfyJRdk&ab_channel=LofiGirl"
+  
   const { data: session } = useSession()
 
   
@@ -59,13 +53,14 @@ export default function Index(props:Props) {
                 <div className='text-center flex flex-col justify-center m-10 border-double border-4 border-cyan-800'>
                   <h1 className='font-extrabold text-6xl '>WEEK CHALLENGE</h1>
                   <div className=' w-2/5 my-5 mx-auto h-[270px] '>
-                    <iframe className="w-full h-full" src={video} title="Saw en Gusano | Gimbarr" frameBorder="0" ></iframe>
+                    <iframe className="w-full h-full" src={videoURL(youtubeUrl)} title="Saw en Gusano | Gimbarr" frameBorder="0" ></iframe>
+                    
                   </div>
                 </div>
 
                 {/* List Videos videos */}
                 <div className="page">
-                  <h1>Public Feed</h1>
+                  <h1 className="font-bold text-gray-800 text-lg">Public Feed</h1>
                   <main>
                     {props.feed.map((post) => (
                       <div key={post.id} className="post">
