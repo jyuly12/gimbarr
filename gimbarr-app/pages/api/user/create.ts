0fd1e85d73c4de11d97d1665e42bd55ac2cfccc9
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-09-15 23:42:13 -0500

    Add create profile functions

--- gimbarr-app/pages/api/user/create.ts
+++ gimbarr-app/pages/api/user/create.ts
@@ -4,19 +4,17 @@ import { getSession } from 'next-auth/react'
 
 
 // POST /api/post
-// Required fields in body: title, video
+// Required fields in body: title
 // Optional fields in body: content
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-  const { title, content, video } = req.body;
+  const { username } = req.body;
 
   const session = await getSession({ req });
   if (session) {
-    const result = await prisma.post.create({
+    const result = await prisma.profile.create({
       data: {
-        title,
-        content,
-        videoclip: video,
-        author: { connect: { email: session?.user?.email } },
+        username: username,
+        user: { connect: { email: session?.user?.email } },
       },
     });
     res.json(result);
--- gimbarr-app/pages/profile/create.tsx
+++ gimbarr-app/pages/profile/create.tsx
@@ -1,26 +1,60 @@
-import { useSession, signIn, signOut } from "next-auth/react"
-import type { GetServerSideProps } from "next";
 
+import React, { useState } from "react";
+import Router from "next/router";
 
+const CreateData: React.FC = () => {
+  const [username, setUsername] = useState("");
 
+  const submitData = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    try {
+      const body = { username };
+      await fetch(`/api/user/create`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      await Router.push("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-type Props = {
+  return (
+    <>
+      <div className="h-screen w-screen bg-slate-300 flex place-items-center place-content-center">
+        
+        <form onSubmit={submitData} className="bg-white w-1/3 shadow-xl p-5 rounded-2xl ">
+
+          <h1 className="capitalize font-semibold text-lg">please put a username</h1>
+
+          {/* Post Username */}
+          <input
+            autoFocus
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="username"
+            type="text"
+            value={username}
+            className="w-full p-2 my-5 border-4 border-gray-300 outline-cyan-700 rounded-xl"
+          />
+          
+        <div className="my-5">
+          {/* Create Button */}
+          <input disabled={!username} type="submit" value="Create" 
+          className="bg-gray-200 rounded-xl py-3 px-4  hover:bg-slate-300"/>
+          
+          {/* when cancel redirect to*/}
+          <a className="ml-4" href="#" onClick={() => Router.push("/")}>
+            or Cancel
+          </a>
+        </div>
+        </form>
+          
+        
+
+      </div>
+    </>
+  );
 };
 
-export default function Index(props:Props) {
-
-  /* videojs */
-  const youtubeUrl = "https://www.youtube.com/watch?v=jfKfPfyJRdk&ab_channel=LofiGirl"
-  
-  const { data: session } = useSession()
-
-  
-  if (!session) {
-
-  }
-    return (
-        <>       
-          <div>asdkljashdka</div>
-        </>
-    )  
-} 
+export default CreateData;
