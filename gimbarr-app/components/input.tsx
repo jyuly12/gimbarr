5a7fbf8fdd563f45ed0f5f074af01fbfb78ca9a2
jyuly12 <yuly.gonzalez2021@gmail..com>
2022-09-15 21:23:46 -0500

    input component update

--- gimbarr-app/components/input.tsx
+++ gimbarr-app/components/input.tsx
@@ -1,6 +1,7 @@
 interface input{
     id: string,
     name: string,
+    value: string,
     rightroute?: string | null,
     rightlabel?: string | null,
     type: string,
@@ -20,7 +21,7 @@ export default function Input(props:input) {
             {/* Input */}
             <div>
 
-                <input type={props.type} name={props.name} id={props.id} placeholder={props.placeholder} 
+                <input type={props.type} name={props.name} id={props.id} placeholder={props.placeholder} value={props.value}
                 className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border 
                 border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 
                 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
